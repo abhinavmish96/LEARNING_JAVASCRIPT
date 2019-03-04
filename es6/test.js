11061199e335572a86456348ea0986eeb2484c2e
Abhinav Mishra <abhinavmish96@gmail.com>
2019-03-04 22:27:31 +0530

    <On a Spree>

--- es6/test.js
+++ es6/test.js
@@ -7,7 +7,7 @@ const sentences = [
 //es6 feature: object destructuring
 function say({subject, verb, object}){
     //es6 feature: template strings
-    console.log('${subject} ${verb} ${object}');
+    console.log(`${subject} ${verb} ${object}`);
 }
 //es6 feature: for..of
 for(let s of sentences) {
--- js/practise.js
+++ js/practise.js
@@ -1,7 +1,7 @@
 console.log("Hello World!");
 
 function rand(m,n){
-    return m + Math.floor((n-m+1)*Math.randam());
+    return m + Math.floor((n-m+1)*Math.random());
 }
 
 function randFace(){
@@ -10,9 +10,12 @@ function randFace(){
 }
 
 let funds = 50;
+let round =0;
 
 while(funds > 1 && funds < 100){
 
+    round++;
+    console.log(`\tround: ${round}`);
     //placing bets 
     const bets = { crown :0, anchor :0, heart :0, spade:0, club:0, diamond:0};
     let totalBet = rand(1,funds);
@@ -26,8 +29,8 @@ while(funds > 1 && funds < 100){
             let bet = rand(1,remaining);
             let face = randFace();
             bets[face] = bets[face]+bet;
-            remaining = remainig - bet;
-        } while(remainig > 0);
+            remaining = remaining - bet;
+        } while(remaining > 0);
     }
     funds = funds - totalBet;
 
@@ -36,5 +39,15 @@ while(funds > 1 && funds < 100){
     for(let roll = 0; roll < 3;roll++){
         hand.push(randFace());
     }
+
+    // collect winning
+    let winnings = 0;
+    for(let die = 0; die < hand.length; die++){
+        let face = hand[die];
+        if(bets[face]>0) winnings = winnings + bets[face];
+    }
+    funds = funds + winnings; 
+    console.log(`\twinnigs: ${winnings}`);
+    console.log(`\tending funds: ${funds}`);
 
 }
