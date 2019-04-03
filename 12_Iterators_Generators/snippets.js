18258514667c1a98d5bd44f05e3f7215d00b1793
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-03 16:28:59 +0530

    < Update 12>

--- 12_Iterators_Generators/snippets.js
+++ 12_Iterators_Generators/snippets.js
@@ -6,3 +6,16 @@
 const book = ["Twinkle, twinkle, little bat!" , "How I wonder what you're at!" , "Up above the world you fly," , "Like a tea tray in the sky." , "Twinkle, twinkle, little bat!" , "How I wonder what you're at!"];
 const it = book.values();
 it.next();
+
+
+/* While loop over array using iterator */
+const book = ["Twinkle, twinkle, little bat!" , "How I wonder what you're at!" , "Up above the world you fly," , "Like a tea tray in the sky." , "Twinkle, twinkle, little bat!" , "How I wonder what you're at!"];
+const it = book.values();
+let current = it.next();
+while(!current.done){
+    console.log(current.value);
+    current = it.next();
+}
+
+
+/* */
