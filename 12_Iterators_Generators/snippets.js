5fa3b3621cc8ca3943f2b514ab6a5014f82d250b
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-03 16:08:43 +0530

    < Update 12>

--- 12_Iterators_Generators/snippets.js
+++ 12_Iterators_Generators/snippets.js
@@ -2,3 +2,7 @@
 
 //All the code snippets to be run in browser console for best results
 
+/* Iteraor value method */
+const book = ["Twinkle, twinkle, little bat!" , "How I wonder what you're at!" , "Up above the world you fly," , "Like a tea tray in the sky." , "Twinkle, twinkle, little bat!" , "How I wonder what you're at!"];
+const it = book.values();
+it.next();
