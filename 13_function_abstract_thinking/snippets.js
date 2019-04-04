8034b83326f020daa31d016b9362a6033da75e64
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-04 17:24:30 +0530

    < Update Chap 13 >

--- 13_function_abstract_thinking/snippets.js
+++ 13_function_abstract_thinking/snippets.js
@@ -2,4 +2,11 @@
 
 //All the code snippets to be run in browser console for best results
 
-/* */
+/* Function as Subroutine */
+function printLeapYearstatus(){
+    const year = new Date().getFullYear();
+    if(year % 4 !== 0) console.log(`${year} is NOT a leap year`);
+    else if(year % 100 != 0) console.log(`${year} is a leap year`);
+    else if(year % 400 != 0) console.log(`${year} is NOT a leap year`);
+    else console.log(`${year} is a leap year`);
+}
