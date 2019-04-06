e04b9c848c89f3497968f55f84abc8a66a5467b5
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-06 16:00:49 +0530

    < Update Chap 13 >

--- 13_function_abstract_thinking/snippets.js
+++ 13_function_abstract_thinking/snippets.js
@@ -33,3 +33,11 @@ function getNextRainbowColor(){
 }
 
 // Example of a pure function
+function isLeapYear(year){
+    if(year % 4 !== 0) return false;
+    else if(year % 100 != 0) return true;
+    else if(year % 400 != 0) return false;
+    else return true;
+}
+
+//
