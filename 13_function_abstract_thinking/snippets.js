c92ed4fff5cc62ab01058e6ba1362c4072db81b7
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-08 17:24:03 +0530

    < Update Chap 13 >

--- 13_function_abstract_thinking/snippets.js
+++ 13_function_abstract_thinking/snippets.js
@@ -160,3 +160,9 @@ function findNeedle(haystack) {
     return findNeedle(haystack);
 }
 findNeedle(['hay', 'hay', 'hay', 'hay', 'needle', 'hay', 'hay']);
+
+// factorial recursion
+function fact(n){
+    if(n === 1 || n === 0 ) return 1;
+    return n*fact(n-1);
+}
