385b0cbdbe776d6f3b3f6cab46ba0a6c1b8cd0e2
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-08 12:29:50 +0530

    < Update Chap 13 >

--- 13_function_abstract_thinking/snippets.js
+++ 13_function_abstract_thinking/snippets.js
@@ -58,6 +58,14 @@ function getRaibowIterator(){
 setTimeout(function(){console.log('hello');}, 1500);
 
 // using setTimeout() with different time interval without scoping problem
+var i;
+for(i = 5; i >= 0 ; i--){
+        setTimeout( function(){
+            console.log(i===0 ? "go!" : i);
+        }, (5 - i)*1000);
+}
+
+// using setTimeout() with different time interval without scoping problem
 for(let i = 5; i >= 0 ; i--){
     setTimeout( function(){
         console.log(i===0 ? "go!" : i);
