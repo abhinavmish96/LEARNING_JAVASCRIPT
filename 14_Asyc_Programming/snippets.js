3dc13f5050b9713fe11f9003a650e606a70caa1c
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-09 17:52:00 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -76,3 +76,15 @@ fs.readFile('a.txt', function(err, dataA){
 });
 
 //try..catch failure
+const fs = require('fs');
+function readSketchyFile(){
+    try{
+        fs.readFile('does_not_exist.txt', function(err, data){
+            if(err) throw err;
+        }); 
+    } catch(err){
+        console.log('warning: minor issue occured, program counting');
+    }
+}
+reaSketchyFile();
+
