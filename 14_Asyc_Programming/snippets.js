f0ef09f32a89769d5a84c3d418248766cbb1f04a
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-09 12:54:41 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -2,4 +2,13 @@
 
 //All the code snippets to be run in browser console for best results
 
-/*  */
+/* Callbacks */
+
+// setTimeout function as callback
+console.log('Before timeout: ' + new Date());
+function f(){
+    console.log('After timeout: ' + new Date());
+}
+setTimeout(f, 60*1000); // one minute
+console.log('I happen after setTimeout!');
+console.log('Me too!');
