28b81b2404130084bd8a87e1337cab34585c3be8
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-09 13:41:04 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -12,3 +12,12 @@ function f(){
 setTimeout(f, 60*1000); // one minute
 console.log('I happen after setTimeout!');
 console.log('Me too!');
+
+/* setInterval and clearInterval */
+const start = new Date();
+let i = 0;
+const intervalId = setInterval(function(){
+    let now = new Date();
+    if(now.getMinutes() != start.getMinutes() || ++i>10) return clearInterval(intervalId);
+    console.log(`${i} : ${now}`);
+}, 5*1000);
