a1259870e1b67b2df572dfd4f41a7edfae77ecea
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-09 13:52:16 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -34,3 +34,14 @@ function countdown(){
     }
 }
 countdown();
+
+// 5-second countdown correct result
+function countdown(){
+    console.log('Countdown:');
+    for(let i = 5; i >= 0; i-- ){
+        setTimeout(function(){
+            console.log(i==0 ? 'Go!' : i);
+        }, (5-i)*1000);
+    }
+}
+countdown();
