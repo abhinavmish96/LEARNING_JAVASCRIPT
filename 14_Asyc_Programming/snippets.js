c8e5931d887ba024b2dcca6a77ccb9af5b552018
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-10 15:20:13 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -171,3 +171,50 @@ c.go()
     .catch(function(err){
         console.error(err.message);
     })
+
+
+
+/* Promise chaining */
+
+const EventEmitter = require('events').EventEmitter;
+
+class Countdown extends EventEmitter {
+    constructor(seconds, superstitious) {
+        super();
+        this.seconds = seconds;
+        this.superstitious = superstitious;
+    }
+    go(){
+        const countdown = this;
+        return new Promise(function (resolve, reject){
+            for(let i = countdown.seconds; i>=0; i--) {
+                setTimeout(function(){
+                    if(countdown.supertitious && i===13) return reject(new Error('DEFINITELY NOT COUNTING THAT'));
+                    countdown.emit('tick', i);
+                    if(i===0) resolve();
+                }, (countdown.seconds - i)*1000);
+            }
+        });
+    }
+}
+
+function launch(){
+    return new Promise(function(resolve, reject){
+        console.log('Lift off!');
+        setTimeout(function(){
+            resole('In orbit!');
+        }, 2*1000);
+    });
+}
+
+const c = new Countdown(5)
+    .on('tick', i => console.log(i+'...'));
+
+c.go()
+    .then(launch)
+    .then(function(msg){
+        console.log(msg);
+    })
+    .catch(function(err){
+        console.error(err.message);
+    })
