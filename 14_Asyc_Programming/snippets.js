435362a2f8c216528459053e59b8ce97827f98b4
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-10 17:47:34 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -259,3 +259,10 @@ function* theFutureIsNow(){
     yield nfcall(fs.writeFile, 'd.txt', dataA+dataB+dataC);
 }
 grun(theFutureIsNow);
+
+//Promise.all
+function* theFutureIsNow(){
+    const data = yield Promise.all([nfcall(fs.readFile, 'a.txt'), nfcall(fs.readFile, 'b.txt'), nfcall(fs.readFile, 'c.txt')]);
+    yield ptimeout(60*1000);
+    yield nfcall(fs.writeFile, 'd.txt', dataA+dataB+dataC);
+}
