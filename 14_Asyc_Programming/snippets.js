990488b6a2d4f0796016f574edeb5032e0389cfe
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-10 14:35:59 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -136,3 +136,26 @@ p.then(function(){
 p.catch(function(err){
     console.log("countdowm experinced an error:" + err.message);
 });
+
+/* Events */
+const EventEmitter = require('events').EventEmitter;
+
+class Countdown extends EventEmitter {
+    constructor(seconds, superstitious) {
+        super();
+        this.seconds = seconds;
+        this.superstitious = superstitious;
+    }
+    go(){
+        const countdown = this;
+        return new Promise(function (resolve, reject){
+            for(let i = countdown.seconds; i>=0; i--) {
+                setTimeout(function(){
+                    if(countdown.supertitious && i===13) return reject(new Error('DEFINITELY NOT COUNTING THAT'));
+                    countdown.emit('tick', i);
+                    if(i===0) resolve();
+                }, (countdown.seconds - i)*1000);
+            }
+        });
+    }
+}
