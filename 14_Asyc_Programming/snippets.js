606fc983fd0ef4e0f2c180edd3b8218aa6ff1437
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-10 13:00:01 +0530

    < Update Chap 14 >

--- 14_Asyc_Programming/snippets.js
+++ 14_Asyc_Programming/snippets.js
@@ -88,3 +88,14 @@ function readSketchyFile(){
 }
 reaSketchyFile();
 
+/* creating promises */
+function countdown(seconds){
+    return new Promise(function (resolve, reject){
+        for(let i = seconds; i>=0; i++) {
+            setTimeout(function(){
+                if(i>0) console.log(i+'...');
+                else resolve(console.log('Go!'));
+            }, (seconds - i)*1000);
+        }
+    });
+}
