4039ece8df628b21043be95cd1b0164e3d4f2b64
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-11 15:22:37 +0530

    < Update Chap 15 >

--- 15_Date_Time/snippets.js
+++ 15_Date_Time/snippets.js
@@ -103,5 +103,18 @@ const msDiff = d2 - d1; // ms
 const daysDiff = msDiff/1000/60/60/24; // days
 
 // sort array using Array.protoype.sort
+const dates = [];
+// create some random dates
+const min = new Date(2017,0,1).valueOf();
+const delta = new Date(2020,0,1).valueOf() - min;
+for(let i=0; i<10; i++)
+dates.push(new Date(min + delta*Math.random()));
+// dates are random probably jumbled
+// we can sort them (descending):
+dates.sort((a,b) => b - a);
+// or ascending
+dates.sort((a,b) => a - b);
+
+
 
 
