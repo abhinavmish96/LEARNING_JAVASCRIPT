306976f6e4c71ef6f71068a7c25fb3c6e8bb46b1
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-11 13:25:32 +0530

    < Update Chap 15 >

--- 15_Date_Time/snippets.js
+++ 15_Date_Time/snippets.js
@@ -42,3 +42,14 @@ const after = JSON.parse(json);
 after.d instanceof Date; // false
 typeof after.d //string
 
+// recovering a date
+after.d = new Date(after.d);
+after.d instanceof Date; // true
+
+// numeric valueof the date
+const before = { d: new Date().valueOf()};
+typeof before.d  // "number"
+const json = JSON.stringify(before);
+const after = JSON.parse(json);
+typeof after.d  // "number"
+const d = new Date(after.d);
