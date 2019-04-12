66c00415970fd8d403390c1527633d62d8aadb66
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-12 12:32:12 +0530

    < Update Chap 16 >

--- 16_Math/snippets.js
+++ 16_Math/snippets.js
@@ -16,3 +16,13 @@ x.toFixed(0); // "20"
 // o/p is not truncated instead rounded
 
 /* Exponential Notation */
+const x = 3800.5;
+x.toExponential(4); // "3.8005e+3"
+x.toExponential(3); // "3.801e+3"
+x.toExponential(2); // "3.80e+3"
+x.toExponential(1); // "3.8e+3"
+x.toExponential(0); // "4e+3"
+
+// o/p is not truncated instead rounded
+// the specified precision is the number of digits past the decimal
+
