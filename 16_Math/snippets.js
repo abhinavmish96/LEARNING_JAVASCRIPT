0515f52794167f1f232be2da2b649f55486917a7
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-12 12:27:20 +0530

    < Update Chap 16 >

--- 16_Math/snippets.js
+++ 16_Math/snippets.js
@@ -5,3 +5,14 @@
 // this chapter describes JS built-in Math object
 
 /* Fixed Decimal */
+// use of Number.protoype.toFixed
+const x = 19.51;
+x.toFixed(3); // "19.510"
+x.toFixed(2); // "19.51"
+x.toFixed(1); // "19.5"
+x.toFixed(0); // "20"
+
+// the o/p is in string form to preserve format
+// o/p is not truncated instead rounded
+
+/* Exponential Notation */
