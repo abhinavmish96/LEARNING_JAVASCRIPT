1df2ec410d00ed3f53fb2bcd65a1198d6690e426
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-12 12:38:35 +0530

    < Update Chap 16 >

--- 16_Math/snippets.js
+++ 16_Math/snippets.js
@@ -26,3 +26,20 @@ x.toExponential(0); // "4e+3"
 // o/p is not truncated instead rounded
 // the specified precision is the number of digits past the decimal
 
+/* Fixed Precision */
+let x = 1000;
+x.toPrecision(5); // "1000.0"
+x.toPrecision(4); // "1000"
+x.toPrecision(3); // "1.00e+3"
+x.toPrecision(2); // "1.0e+3"
+x.toPrecision(1); // "1e+3"
+x = 15.335;
+x.toPrecision(6); // "15.3350"
+x.toPrecision(5); // "15.335"
+x.toPrecision(4); // "15.34"
+x.toPrecision(3); // "15.3"
+x.toPrecision(2); // "15"
+x.toPrecision(1); // "2e+1"
+
+// o/p is rounded and will always have the specified number of digits of precision
+
