c6d42ae86cb646357d00d7d6da630fb30c05a057
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-15 15:51:09 +0530

    < Update Chap 17 >

--- 17_Reg_Expressions/snippets.js
+++ 17_Reg_Expressions/snippets.js
@@ -59,3 +59,12 @@ const match = html.match(/area|a|link|script|soutce/ig); // first attempt
 /* Matching HTML */
 const html = '<br> [!CDATA[[<br>]]';
 const matches = html.match(/<br>/ig);
+
+/* Character Sets */
+const berr99 = "99 bottles of beer on the wall " + "take 1 down and pass it around -- " + "98 bottles of beer on the wall.";
+const matches = berr99.match(/0||1|2|3|4|5|6|7|8|9/g); // tedious
+const m1 = beer99.match(/[0123456789]/g); //okay
+const m2 = beer99.match(/[0-9]/g); // better!
+
+// combining ranges
+const match = beer99.match(/[-0-9a-z.]/ig);
