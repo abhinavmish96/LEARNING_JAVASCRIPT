80fe6dd963d93a129322d4e8255c169333798e3b
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-15 16:00:03 +0530

    < Update Chap 17 >

--- 17_Reg_Expressions/snippets.js
+++ 17_Reg_Expressions/snippets.js
@@ -82,4 +82,6 @@ const levels = stuff.match(/:\s*[0-9]/g);
 const messyPhone = '(505) 555-1515';
 const neatPhone = messyPhone.replace(/\D/g, '');
 
-//
+//using \S
+const field = '     something';
+const valid = /\S/.test(field);
