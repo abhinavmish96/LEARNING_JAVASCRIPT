b6c50958ca130686cae578ec60ac70062f595aa0
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-15 12:08:19 +0530

    < Update Chap 17 >

--- 17_Reg_Expressions/snippets.js
+++ 17_Reg_Expressions/snippets.js
@@ -3,3 +3,14 @@
 //All the code snippets to be run in browser console for best results
 
 /* Substring Matching and Replacing */
+//String.protoype
+const input = "As I was going to Saint Ivens";
+input.startsWith("As"); // true
+input.endsWith("Ivens"); // true
+input.startsWith("going", 9); // true -- starts at index 9
+input.endsWith("going", 14); // true -- treat index 14 as the end of the string
+input.includes("going"); // true
+input.includes("going", 10); // false -- starting at index 10
+input.indexOf("going"); // 9
+input.indexOf("going", 10); // -1
+input.indexOf("nope"); // -1
