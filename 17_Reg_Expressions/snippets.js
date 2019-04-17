952aa9112a83a9ea2e35210eb6b9912cdcebe4d9
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-17 15:29:35 +0530

    < Update Chap 17 >

--- 17_Reg_Expressions/snippets.js
+++ 17_Reg_Expressions/snippets.js
@@ -197,3 +197,8 @@ const inputs = [
 ];
 const emailMatcher = /\b[a-z][a-z0-9._-]*@[a-z][a-z0-9_-]+\.[a-z]+(?:\.[a-z]+)?\b/ig;
 inputs.map( s => s.replace(emailMatcher, '<a href="mailto:$&">$&</a>')); // (5) ["<a href="mailto:john@doe.com">john@doe.com</a>", "<a href="mailto:john@doe.com">john@doe.com</a> is my email", "my email is <a href="mailto:john@doe.com">john@doe.com</a>", "use <a href="mailto:john@doe.com">john@doe.com</a>, my email", "my email:<a href="mailto:john@doe.com">john@doe.com</a>."]
+
+/* Lookaheads */
+function validPassword(p){
+    return /(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?!.*[^a-zA-Z0-9])/.test(p);
+}
