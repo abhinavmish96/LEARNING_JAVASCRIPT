d4681a2a20a4e0b3e5fd2ef05d4de683d9f1eafa
Abhinav Mishra <abhinavmish96@gmail.com>
2019-04-19 12:26:00 +0530

    < Update Chap 18 >

--- 18_JavaScript_in_the_Browser/snippets.js
+++ 18_JavaScript_in_the_Browser/snippets.js
@@ -2,4 +2,11 @@
 
 //All the code snippets to be run in browser console for best results
 
-/*  */
+/* function that traverses the entire DOM and prints it to the console */
+function printDOM(node, prefix){
+    console.log(prefix + node.nodeName);
+    for(let i=0; i<node.childNodes.length; i++){
+        printDOM(node.childNodes[i], prefix + '\t');
+    }
+}
+printDOM(document, '');
