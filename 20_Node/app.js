bf0c42c2f688d7929548e2d56b4b4adbd37a648c
Abhinav Mishra <abhinavmish96@gmail.com>
2019-05-22 14:07:56 +0530

    <Chap 20>

--- 20_Node/app.js
+++ 20_Node/app.js
@@ -1,5 +1,10 @@
 const amanda_calculate = require('./amanda.js');
 const tyler_calculate = require('./tyler.js');
+const func = require('./func.js')
 
 console.log(amanda_calculate(1,2,5)); // 31
 console.log(tyler_calculate(2)); // 33.510321638291124
+
+console.log(func.geometricSum(1,2,5)); // 31
+console.log(func.quadraticFormula(1, 2, -15)); // [3, -5]
+console.log(func.arithmeticSum(5)); // 15
--- 20_Node/func.js
+++ 20_Node/func.js
@@ -1,10 +1,10 @@
 module.exports = {
     geometricSum(a, x, n){
         if(x === 1) return a*n;
-        return a*(1 - Math.pow(x/n))/(1-x);
+        return a*(1 - Math.pow(x, n))/(1 - x);
     },
     arithmeticSum(n){
-        return (n+1)*n/2;
+        return (n + 1)*n/2;
     },
     quadraticFormula(a, b, c){
         const D = Math.sqrt(b*b - 4*a*c);
