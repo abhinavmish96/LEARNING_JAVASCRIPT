6163c04255023fc578fb17678a72a341a75bc92f
Abhinav Mishra <abhinavmish96@gmail.com>
2019-06-03 13:43:13 +0530

    <Chap 21>

--- 21_OPCandP/descrip.js
+++ 21_OPCandP/descrip.js
@@ -8,3 +8,12 @@ Enumerable
 Controls whether the property will be included when the properties of the object are enumerated (with for...in, Object.keys, or the spread operator).
 Configurable
 Controls whether the property can be deleted from the object or have its attributes modified. */
+
+Object.defineProperty(obj, 'foo' , { writable: false});
+
+console.log(Object.getOwnPropertyDescriptor(obj, 'foo'));
+
+obj.foo = 3;
+
+/*Attempting to set a read-only property will only result in an error in strict mode. In nonstrict mode, the assignment will not be suc‐ cessful, but there will be no error.*/
+
